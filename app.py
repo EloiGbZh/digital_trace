2665d5bd9d14a6491b48c87808d9bdf98b8dddaf
egravot <eloi.gravot@epfedu.fr>
2022-11-29 15:28:51 +0100

    third commit

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask
 
+def hello_world():
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=344237591"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', ' 344237591');
+    </script>
+    """
+    return prefix_google + "Hello World"
+
+
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
