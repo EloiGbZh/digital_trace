b3b59934f570ce59734c191adb5017c31c7a1d0f
egravot <eloi.gravot@epfedu.fr>
2022-11-29 16:11:24 +0100

    againnnn

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def hello_world():
     prefix_google = """
     <!-- Google tag (gtag.js) -->
     <script async
-    src="https://www.googletagmanager.com/gtag/js?id=344237591"></script>
+    src="https://www.googletagmanager.com/gtag/js?id=G-KG1PQW1MWX"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', '344237591');
+    gtag('config', 'G-KG1PQW1MWX');
     </script>
     """
     return prefix_google + "Hello World"
