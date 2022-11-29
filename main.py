eeebd1eed1cbec582cf5814a9f0994f6bf8b10d0
egravot <eloi.gravot@epfedu.fr>
2022-11-29 16:06:52 +0100

    again

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def hello_world():
     prefix_google = """
     <!-- Google tag (gtag.js) -->
     <script async
-    src="https://www.googletagmanager.com/gtag/js?id=4320752133"></script>
+    src="https://www.googletagmanager.com/gtag/js?id=344237591"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', '4320752133');
+    gtag('config', '344237591');
     </script>
     """
     return prefix_google + "Hello World"
